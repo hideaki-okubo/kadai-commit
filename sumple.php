163cedb5820021d4e072eaf3c0e45f8389ce3fae
hideaki.okubo <hidewaki0222@gmail.com>
2019-10-04 01:36:46 +0000

    add sumple.php

---
+++ sumple.php
@@ -0,0 +1,14 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lung="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：なし</p>
+        <form action="sumple.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
+        </form>
+    </body>
+</html>
